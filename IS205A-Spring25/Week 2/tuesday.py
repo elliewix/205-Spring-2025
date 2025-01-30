497aa7db8d75c98198b83e307812486ab838b6f2
Elizabeth Wickes <elliewix@gmail.com>
2025-01-30 09:24:43 -0600

    tuesday

---
+++ IS205A-Spring25/Week 2/tuesday.py
@@ -0,0 +1,22 @@
+print("hello again")
+
+print(4)
+print("4")
+print(4.0)
+
+"hello" # nothing will show up
+# another comment here
+# the following will actually print out
+print("hello")
+
+# operators
+## plus operator, int + int = int, addition
+print(1 + 1)
+print(1 + 1.0)
+print(3.0 + 1)
+
+## we can usually do more
+
+print("cat" + "cat")
+## but not always
+print("cat" + 1)
---
+++ IS205B-Spring25/Week 02/Tuesday.py
@@ -0,0 +1,61 @@
+print("hello")
+
+# data types
+print(1)
+print("1")
+print(1.0)
+
+# operators
+
+print(1 + 1)
+print(1.0 + 1.0)
+print(1.0 + 1)
+
+# you can extend to other types
+## not always
+# print("cat" + 1) # won't work, str and int
+print("this is accurate tho")
+
+print("cat" + "cat")
+
+# boolean values
+print(True)
+print(False)
+
+# diff than
+print("True")
+print("False")
+print("false")
+
+## python has eyes of its own
+## things are still happening
+1 + 1
+"cat" + "cat"
+# but to see the output you need print()
+
+## variables
+## variable names: there are some rules
+
+number = 5
+
+# maybe like coords like (x,y)
+x = 5
+y = 5
+print(x)
+print(y)
+print(x + y)
+
+# print("cat" + x)
+
+## but you to give varibales a decent name
+## something meaningful
+
+total = 0
+
+# calculate the mean/avg
+
+n = 10
+total = 50
+mean = total/n
+print("my average is")
+print(mean)
